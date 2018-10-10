e7939e1ee5ab59bd5653364acf36e28de97f8909
Erison <erivaps@gmail.com>
2018-10-09 20:06:51 -0500

    first commit

--- app/src/main/java/com/kotlin/operadores/calculaldora/caluladora/MainActivity.kt
+++ app/src/main/java/com/kotlin/operadores/calculaldora/caluladora/MainActivity.kt
@@ -2,11 +2,27 @@ package com.kotlin.operadores.calculaldora.caluladora
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //directo desde el boton
+        btnSumar.setOnClickListener {
+            var primero = txtOpe1.text
+            var segundo = txtOpe2.text
+            txtresultado.setText(primero + segundo)
+        }
+    }
+
+    fun sumar(view : View){
+        var primero = txtOpe1.text
+        var segundo = txtOpe2.text
+        txtresultado.setText(primero + segundo)
     }
 }
