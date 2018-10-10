537321e83ec38eb6493edbf47e2c6c240013a097
Erison <erivaps@gmail.com>
2018-10-09 20:33:05 -0500

    first commit

--- app/src/main/java/com/kotlin/operadores/calculaldora/caluladora/MainActivity.kt
+++ app/src/main/java/com/kotlin/operadores/calculaldora/caluladora/MainActivity.kt
@@ -13,16 +13,38 @@ class MainActivity : AppCompatActivity() {
 
 
         //directo desde el boton
-        btnSumar.setOnClickListener {
-            var primero = txtOpe1.text
-            var segundo = txtOpe2.text
-            txtresultado.setText(primero + segundo)
-        }
+       /* btnSumar.setOnClickListener {
+            var primero = txtOpe1.text.toString()
+            var segundo = txtOpe2.text.toString()
+            txtresultado.setText(primero.toInt() + segundo.toInt())
+        }*/
     }
 
     fun sumar(view : View){
-        var primero = txtOpe1.text
-        var segundo = txtOpe2.text
-        txtresultado.setText(primero + segundo)
+        var primero = txtOpe1.text.toString()
+        var segundo = txtOpe2.text.toString()
+        var resultado = primero.toInt() + segundo.toInt()
+        txtresultado.setText(resultado.toString())
+    }
+
+    fun restar(view : View){
+        var primero = txtOpe1.text.toString()
+        var segundo = txtOpe2.text.toString()
+        var resultado = primero.toInt() - segundo.toInt()
+        txtresultado.setText(resultado.toString())
+    }
+
+    fun multiplicar(view : View){
+        var primero = txtOpe1.text.toString()
+        var segundo = txtOpe2.text.toString()
+        var resultado = primero.toInt() * segundo.toInt()
+        txtresultado.setText(resultado.toString())
+    }
+
+    fun dividir(view : View){
+        var primero = txtOpe1.text.toString()
+        var segundo = txtOpe2.text.toString()
+        var resultado = primero.toInt() / segundo.toInt()
+        txtresultado.setText(resultado.toString())
     }
 }
